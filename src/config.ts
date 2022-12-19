284ddc05c1da655ba8d9700f4a2b7b8a0325a490
Joshua Byrd <phocks@gmail.com>
2022-12-20 09:03:45 +1000

    Placeholder

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'phocks[...].onion blog';
+export const SITE_TITLE = 'phocks[...].onion';
 export const SITE_DESCRIPTION = 'Yet another blog by phocks but this time on the Tor network';
